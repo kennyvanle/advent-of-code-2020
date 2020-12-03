40257d383933ec74d202553af5b7d7693aa3606a
Kenny Le <kennyle@gatech.edu>
2020-12-03 16:25:54 -0500

    day1 efficiency

--- src/main/java/handler/Day1Handler.java
+++ src/main/java/handler/Day1Handler.java
@@ -1,16 +1,22 @@
 package main.java.handler;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Day1Handler {
 
     public static int day1part1(List<Integer> list){
         int product = 0;
+        Collections.sort(list);
         for(int i=0; i < list.size(); i++){
             for(int j = i+1; j<list.size();j++){
-                if(2020 - list.get(i) - list.get(j) == 0){
+                int diff = 2020 - list.get(i) - list.get(j);
+                if(diff == 0){
                     product = list.get(i) * list.get(j);
                     return product;
+                }
+                if(diff < 0){
+                    break;
                 }
             }
         }
@@ -19,12 +25,17 @@ public class Day1Handler {
 
     public static int day1part2(List<Integer> list){
         int product = 0;
+        Collections.sort(list);
         for(int i=0; i < list.size(); i++){
             for(int j = i+1; j<list.size();j++){
                 for(int k = j+1; k<list.size();k++){
-                    if(2020 - list.get(i) - list.get(j) - list.get(k) == 0){
+                    int diff = 2020 - list.get(i) - list.get(j) - list.get(k);
+                    if(diff== 0){
                         product = list.get(i) * list.get(j) * list.get(k);
                         return product;
+                    }
+                    if(diff < 0){
+                        break;
                     }
                 }
             }
